8afd67ed2bf61bbfe688b9a9a921ea12147ebc65
matthewthein <theinm@uni.edu>
2016-04-12 16:15:01 -0500

    Added makeSale()
    Added makeSale() to the MyToolPOS system!

--- myToolPOS.py
+++ myToolPOS.py
@@ -16,7 +16,32 @@ class connectTools:
     server_info = None
     url = None
     cursor = None
+    counter = 0
+    dateString = str(time.strftime("%Y%m%d"))
 
+
+    def makeSale():
+    global counter
+    counter += 1
+    ticketID = dateString + str(counter)
+    receiptString = ""
+    while True:
+        inputID = input("Scan item or input item ID. Hit enter with no item ID to end sale." )
+        if inputID == "":
+            break
+        else:
+            pass
+            #connect to the database
+            #check item exixts
+            #retrieve item name and price
+            #add to receiptString
+    #submit to sales db
+    #decrement inventory
+    print(ticketID)
+    print("Thanks for shopping at My Tool!")
+    print("Have a great day!")
+    print(receiptString)
+    
     def connect():
         try:
             global server_info
