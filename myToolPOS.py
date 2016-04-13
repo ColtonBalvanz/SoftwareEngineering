81e548834e76456f1958bd3cb3a47b3ba6266427
matthewthein <theinm@uni.edu>
2016-04-13 17:57:30 -0500

    Added comments.
    I added comments to the functions and classes on their basic operations.
    I'll probably add more in depth comments later on how exactly they work
    and what arguments they take.

--- myToolPOS.py
+++ myToolPOS.py
@@ -12,6 +12,14 @@ import time
 
 
 class connectTools:
+##These are some of the variables that will be reused often throughout
+##this class. conn is our variable for the connection called by many of
+##the methods in this class. server_info is a string of the server
+##information stored in a text file outside of our code. The url is the
+##parsed version of the string to work with establishing the connection.
+##The cursor is a tool that will make modifications to the tables in our
+##database.
+
     conn = None
     server_info = None
     url = None
@@ -19,6 +27,10 @@ class connectTools:
 
     
     def connect():
+##The connect method opens the initial connection to the server. To
+##close the connection, call disconnect() after calling connect(). The
+##connection will stay open to call other methods defined in this class.
+        
         try:
             global server_info
             server_info = open('server_info.txt', 'r')
@@ -39,10 +51,20 @@ class connectTools:
             print("Sorry, I am unable to connect to the database")
 
     def disconnect():
-        global conn
-        conn.close()
+##This is called after connect(), and it should not be called before
+##connect().
+
+        try:
+            global conn
+            conn.close()
+        except:
+            print("Sorry, I am not even connected right now. :o")
 
     def query(table, column):
+##This method allows the user to query which table to access and which
+##column in the table. It may be useful, but query_single is more
+##useful. It has the ability to display the whole contents of the table
+##at half the expense of query_single.
 
         global cursor
         cursor.execute("""SELECT %(column)s FROM %(table)s""", {"table": AsIs(table), "column": AsIs(column)})
@@ -50,12 +72,20 @@ class connectTools:
         pprint.pprint(records)
 
     def query_single(table, column, args):
+##This is helpful for when we want to look up an item more specifically.
+##We can look up the price from a barcode and other related columns
+##prior to making a sale.
+        
         global cursor
         cursor.execute("""SELECT %(column)s FROM %(table)s WHERE %(args)s;""", {"table": AsIs(table), "column": AsIs(column), "args": AsIs(args)})
         records = cursor.fetchall()
         pprint.pprint(records)
 
     def modify_single(table, operation, location):
+##modify_single will do the work of subtracting a number of items from a
+##purchase, plus any updates we might have on updating a specific item,
+##such as updating a price or setting the sale of an item.
+        
         global conn
         global cursor
         try:
@@ -66,6 +96,9 @@ class connectTools:
             
             
     def add_sale(row):
+##add_sale has the responsibility to add a row in the sales table, and
+##list items associated with that sale.
+        
         global conn
         global cursor
         try:
@@ -82,6 +115,9 @@ class connectTools:
         
 
     def add_item(table, row):
+##add_item will add a new item to the database. It will add whatever new
+##item My Tool chooses to carry in the store.
+        
 
         global conn
         global cursor
@@ -93,12 +129,19 @@ class connectTools:
         except:
             print("Sorry, I could not add this item")
 
-class saleOperations
+class saleOperations:
+##This new class will outline a different set of responsibilities more
+##abstract from the server. These methods will look more familiar to the
+##end user, but they will ultimately work with methods from the connection class.
 
     counter = 0
     dateString = str(time.strftime("%Y%m%d"))
 
     def makeSale():
+##The makeSale() method is responsible for producing the sale of a
+##customer. It will also print out a physical receipt the customer can
+##take with them.
+        
         global counter
         counter += 1
         ticketID = dateString + str(counter)
@@ -109,11 +152,12 @@ class saleOperations
                 break
             else:
                 pass
-            #connect to the database
+            ##connectTools.query_single()
             #check item exists
             #retrieve item name and price
             #add to receiptString
     #submit to sales db
+    ##connectTools.add_sale()
     #decrement inventory
         print(ticketID)
         print("Thanks for shopping at My Tool!")
@@ -121,10 +165,16 @@ class saleOperations
         print(receiptString)
 
     def decrement(subtract):
+##decrement() changes the quantity of an item in a database. There is
+##not much important about this yet.
         sqlstring = "quantity = quantity - " + subtract
         return sqlstring
 
 def main():
+##This probably won't be here for too long. Once we have a GUI we will
+##have a more functional main() to operate out sale system.
+
+    
     location = "supplier = 'MY TOOL INC'"
     operation = saleOperations.decrement(5)
     column_name = "*"
@@ -137,5 +187,7 @@ def main():
 ##    connectTools.add_item(list1)
     connectTools.disconnect()
     time = makeSale.generate_id()
+
+##This works, I don't know how, but it does.
 if __name__ == '__main__':
     main()
