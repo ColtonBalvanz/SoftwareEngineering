7122991ec4e06103ba4f3749700eb6d69cac416d
Jacob Peterman <peterjbg@uni.edu>
2016-04-22 10:13:25 -0500

    Adjusted font size on buttons
    Increased the font size of the buttons on the right side

--- cashierGUI.py
+++ cashierGUI.py
@@ -10,15 +10,20 @@ def done():
     """Exits the window when 'Exit' button is pressed"""
     root.destroy()
 
-exButton = Button(root,text="Exit",command=done,width=18,height=5,bg="red")
+exButton = Button(root,text="Exit",command=done,width=12,height=6,bg="red",
+                  font="Helvetica 15 bold")
 exButton.place(relx=.91,rely=.08,anchor="c")
-adminView = Button(root,text="Administrative View",width=18,height=5,bg="#c2d6d6")
+adminView = Button(root,text="Administrative View",width=12,height=3,bg="#c2d6d6",
+                   font="Helvetica 15 bold",wraplength=184, justify=CENTER)
 adminView.place(relx=.91,rely=.25,anchor="c")
-voidSale = Button(root,text="Void Sale",width=18,height=5,bg="#c2d6d6")
+voidSale = Button(root,text="Void Sale",width=12,height=3,bg="#c2d6d6",
+                  font="Helvetica 15 bold")
 voidSale.place(relx=.91,rely=.42,anchor="c")
-voidItem = Button(root,text="Void Item",width=18,height=5,bg="#c2d6d6")
+voidItem = Button(root,text="Void Item",width=12,height=3,bg="#c2d6d6",
+                  font="Helvetica 15 bold")
 voidItem.place(relx=.91,rely=.59,anchor="c")
-payNow = Button(root,text="Pay Now!",width=18,height=10,bg="#00ff00")
+payNow = Button(root,text="Pay Now!",width=12,height=6,bg="#00ff00",
+                font="Helvetica 15 bold")
 payNow.place(relx=.91,rely=.835,anchor="c")
 
 text = Text(root, width=14, height=8,font="Helvetica 25 bold")
