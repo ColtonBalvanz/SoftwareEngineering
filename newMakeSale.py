8a13d618f90173aaa17bc841ed968f2afba99dc5
ColtonBalvanz <balvanzc@uni.edu>
2016-05-01 12:19:07 -0500

    more for newMakeSale
    added voidItem and voidSale. May not work yet, need to implement global
    lists for items in Point of Sale system

--- newMakeSale.py
+++ newMakeSale.py
@@ -1,6 +1,26 @@
+from tkinter import *
+#from tkMessageBox import *
+itemList = list()
+
 def newMakeSale(itemID):
     item = connectTools.query_single("inventory", "*", "item_id = " + itemID)
     if item != None:
         return tuple(itemID, item[11], item[9])
     else:
        return tuple(None, None, None)
+
+def voidItem(itemID):
+    global itemList
+    item = connectTools.query_single("inventory", "*", "item_id = " + itemID)
+    if item != None:
+        listItem = tuple([itemID, item[11], item[9]])
+        if listItem in itemList:
+            itemList.remove(listItem)
+        else:
+            showerror("Error", "UPC code not found in cart")
+            
+
+def voidSale():
+    global itemList
+    itemList = list()
+    
