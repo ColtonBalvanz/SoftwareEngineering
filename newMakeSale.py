a527e8dadeda6a7ec89e7ab3cd9d55c558f25c75
ColtonBalvanz <balvanzc@uni.edu>
2016-05-04 15:35:11 -0500

    added endSale
    created an endSale function for when the "Pay Now" button is clicked

--- newMakeSale.py
+++ newMakeSale.py
@@ -7,7 +7,7 @@ def newMakeSale(itemID):
     if item != None:
         return tuple(itemID, item[11], item[9])
     else:
-       return tuple(None, None, None)
+        return tuple(None, None, None)
 
 def voidItem(itemID):
     global itemList
@@ -23,4 +23,33 @@ def voidItem(itemID):
 def voidSale():
     global itemList
     itemList = list()
-    
+
+def endSale():
+    #used when the button "Pay Now!" is clicked
+    global itemList
+    global ticketID
+    global itemNames
+    global itemIDs
+    global itemPrices
+
+    for item in itemList:
+        itemIDs.append(item[0])
+        itemNames.append(item[1]
+        itemPrices.append(item[2])
+    sale = list([int(ticketID), itemIDs, itemPrices])
+    if not connectTools.add_sale(sale)==True:
+        window = tk.Toplevel()
+        window.resizable(width=FALSE,height=FALSE)
+        label = Label(window,text="The system was unable to submit the sale to the sales database. \
+\nPlease contact IT for more information.",font="Helvetica 13 bold")
+        label.pack(side="top",fill="both",padx=10,pady=10)
+        window.after(3000, lambda: window.destroy())
+        window.geometry('{}x{}'.format(480,60))
+    for itemID in itemIDs:
+        connectTools.decrement(1, itemID)
+    window = tk.Toplevel()
+    window.resizable(width=FALSE,height=FALSE)
+    label = Label(window,text="Sale completed!",font="Helvetica 13 bold")
+    label.pack(side="top",fill="both",padx=10,pady=10)
+    window.after(3000, lambda: window.destroy())
+    window.geometry('{}x{}'.format(480,60))
